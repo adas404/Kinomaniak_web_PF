98c682f227adbef8b39505a93a6474931e25fe47
Adam <Adam@Adam-Komputer>
2014-06-11 17:45:54 +0200

    Panel done

--- src/java/kinomaniak/mprimef/WidokSala.java
+++ src/java/kinomaniak/mprimef/WidokSala.java
@@ -11,13 +11,18 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.ws.rs.Produces;
 import kinomaniak.beans.Res;
 import kinomaniak.controllers.BeanManager;
+import org.jboss.logging.Property;
 
 /**
  *
  * @author Adam
  */
+@ManagedBean
 public class WidokSala {
 
     public BeanManager getBeanManager() {
@@ -27,6 +32,7 @@ public class WidokSala {
     public void setBeanManager(BeanManager beanManager) {
         this.beanManager = beanManager;
     }
+        @ManagedProperty("#{beanManager}")
     private BeanManager beanManager;
     /**
      * Creates a new instance of WidokSala
@@ -44,22 +50,15 @@ public class WidokSala {
                 return true;
         return false;
     } 
-    public void decodedRes(int showid){
-        for (Res r: this.getBeanManager().getRes())
-            if (r.getShowID() == showid){
-                Integer.toString(r.getSeat()[0][0]);
-    }
-    }
+  
+    
     List<Integer> selectedOption; 
     public void ustawZaznaczone(){
 
         selectedOption.add(1);
     }
-    @PostConstruct
-    public void init(){
-        selectedOption.add(1);
-    }
-    public WidokSala() {
+    public WidokSala(){
     
-    }
+}
+    
 }
